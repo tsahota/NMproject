b183ffd8197cbeb859a6ab39249cef5525fa3738
Tarjinder Sahota <klgk669@semldxcalvin.seml.astrazeneca.net>
2017-03-12 22:58:37 +0100

    formatR

--- inst/extdata/CodeLibrary/R/group.factor.R
+++ inst/extdata/CodeLibrary/R/group.factor.R
@@ -1,10 +1,8 @@
-## Description: creates group variable
-## Depends on: 
-## Run interactively: FALSE
-## Key words: function
+## Description: creates group variable Depends on: Run interactively: FALSE Key
+## words: function
 
-group.factor <- function(x,n){
-  GRP <- factor(x,levels=unique(x))
-  levels(GRP) <- ((1:length(unique(x))-0.1) %/% n) + 1
-  GRP
+group.factor <- function(x, n) {
+    GRP <- factor(x, levels = unique(x))
+    levels(GRP) <- ((1:length(unique(x)) - 0.1)%/%n) + 1
+    GRP
 }
