a2f060157a3fc37c0a41b553ad0d14f140b78929
Tarjinder Sahota <klgk669@semldxcalvin.seml.astrazeneca.net>
2017-05-11 21:17:08 +0200

    added status to shiny

--- inst/extdata/RunMonitor/server.R
+++ inst/extdata/RunMonitor/server.R
@@ -3,6 +3,10 @@ library(shiny)
 function(input, output, session) {
   session$onSessionEnded(function() stopApp())
   #autoInvalidate <- reactiveTimer(20000, session)
+  output$status <- renderTable({
+    orig.dir <- getwd();  setwd(.currentwd) ; on.exit(setwd(orig.dir))
+    status(.object)
+  })
   output$distPlot <- renderPlot({
     orig.dir <- getwd();  setwd(.currentwd) ; on.exit(setwd(orig.dir))
     #autoInvalidate()
--- inst/extdata/RunTable/server.R
+++ inst/extdata/RunTable/server.R
@@ -3,7 +3,7 @@ library(shiny)
 function(input, output, session) {
   session$onSessionEnded(function() stopApp())
   #autoInvalidate <- reactiveTimer(20000, session)
-  output$run_table <- shiny::renderTable({
+  output$run_table <- renderTable({
     orig.dir <- getwd();  setwd(.currentwd) ; on.exit(setwd(orig.dir))
   #  autoInvalidate()
     run_table()
