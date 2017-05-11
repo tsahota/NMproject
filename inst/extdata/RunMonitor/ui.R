fedcba06c0be362bac0c7c391ed4c64403e6fce2
Tarjinder Sahota <klgk669@semldxcalvin.seml.astrazeneca.net>
2017-05-11 21:17:08 +0200

    added status to shiny

--- inst/extdata/RunMonitor/ui.R
+++ inst/extdata/RunMonitor/ui.R
@@ -2,6 +2,9 @@ library(shiny)
 
 fluidPage(
   titlePanel("Run Monitor"),   # Application title
+  h3("status"),
+  tableOutput("status"),
+  h3("trace"),
   checkboxInput("trans","Transform parameters",TRUE),
   plotOutput("distPlot")
 )
