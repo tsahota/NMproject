91ad706316ed9462b788847d434f2ab76d08c06e
Tarjinder Sahota <klgk669@semldxcalvin.seml.astrazeneca.net>
2017-06-03 01:33:50 +0200

    added aztheopp

--- inst/extdata/examples/aztheopp/Scripts/theopp-demo.R
+++ inst/extdata/examples/aztheopp/Scripts/theopp-demo.R
@@ -12,7 +12,7 @@ source("Scripts/gof1.R")
 ## main script here
 
 ## Initial model
-m1 <- nm("execute run1.mod -dir=1")
+m1 <- nm("qpsn -c auto -t 3000 -- execute run1.mod -dir=1")
 #nm_tran(m1) ## will run NMTRAN check on control stream
 run(m1)
 #shiny_nm() ## will open GUI
@@ -20,24 +20,24 @@ gof1(m1$run_id)
 
 ## Same model for demo purposes
 #copy_control("run1.mod","run2.mod") ## create control file from existing
-m2 <- nm("execute run2.mod -dir=2")
+m2 <- nm("qpsn -c auto -t 3000 -- execute run2.mod -dir=2")
 run(m2)
 gof1(m2$run_id)
 
 ## Bootstrap
-m1boot <- nm("bootstrap run1.mod -samples=10 -dir=1boot")
+m1boot <- nm("qpsn -c auto -t 3000 -- bootstrap run1.mod -samples=10 -dir=1boot")
 run(m1boot)
 
 ## VPC
-m1vpc <- nm("vpc run1.mod -samples=50 -dir=1vpc")
+m1vpc <- nm("qpsn -c auto -t 3000 -- vpc run1.mod -samples=50 -dir=1vpc")
 run(m1vpc)
 
 ## SSE
-m1sse <- nm("sse run1.mod -samples=5 -dir=1sse")
+m1sse <- nm("qpsn -c auto -t 3000 -- sse run1.mod -samples=5 -dir=1sse")
 run(m1sse)
 
 ## SCM
-m1scm <- nm("scm run1.mod -config_file=run1.scm -dir=1scm")
+m1scm <- nm("qpsn -c auto -t 3000 -- scm run1.mod -config_file=run1.scm -dir=1scm")
 run(m1scm)
 
 
