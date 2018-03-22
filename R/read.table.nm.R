b5e57cb53cac36639d477d7aaf88d149ae42ebc2
Tarjinder Sahota <klgk669@semldxzilker.seml.astrazeneca.net>
2018-03-22 14:14:17 +0100

    removed read.table.nm

--- R/read.table.nm.R
+++ R/read.table.nm.R
@@ -4,8 +4,8 @@
 #' @param ... other arguments to be passed to read.table()
 #' @export
 
-read.table.nm <- function(file,...){
-  suppressWarnings(suppressMessages(setAs("character","dummy.numeric", function(from) as.numeric(from))))
-  tmp <- suppressWarnings(read.table(file, fill=T, colClasses="dummy.numeric",...))
-  return(tmp[complete.cases(tmp[,sapply(tmp,function(i) !all(is.na(i)))]),])
-}
+#read.table.nm <- function(file,...){
+#  suppressWarnings(suppressMessages(setAs("character","dummy.numeric", function(from) as.numeric(from))))
+#  tmp <- suppressWarnings(read.table(file, fill=T, colClasses="dummy.numeric",...))
+#  return(tmp[complete.cases(tmp[,sapply(tmp,function(i) !all(is.na(i)))]),])
+#}
