b06032b72cb024f9db603467a673e3d5da3d7e8c
Tarjinder Sahota <klgk669@semldxzilker.seml.astrazeneca.net>
2018-09-03 02:48:28 +0200

    no longer using test_path()

--- tests/testthat/test-plot_iter.R
+++ tests/testthat/test-plot_iter.R
@@ -17,19 +17,19 @@ test_that("db & plot_iter works",{
     cleanup(proj_name)
   })
 
-  testfilesloc <- normalizePath(file.path(test_path(),"testfiles"))
+  testfilesloc <- file.path(currentwd,"testfiles")
   setwd(proj_name)
   file.copy(file.path(testfilesloc,"."),".",recursive = TRUE)
-  
+
   ## end boiler plate
   ############################
 
   m1 <- nm("qpsn -m -c auto -t 3000 -- execute run1.mod -dir=1")
-  
+
   m1again <- nm("qpsn -m -c auto -t 3000 -- execute run1.mod -dir=1")
-  
+
   expect_true(identical(m1, m1again))
-  
+
   expect_true(is_finished(m1)) ## m1 has been pre-run
 
   ## test to see if ctl_character and ctl_list
@@ -42,12 +42,12 @@ test_that("db & plot_iter works",{
   thetasNM <- theta_r2nm(thetasR)
   expect_true(inherits(thetasNM,"nm.theta"))
 
-  ## old way: copy_control 
+  ## old way: copy_control
   copy_control("run1.mod","run3.mod")
   expect_true(file.exists("Models/run3.mod"))
-  
+
   ## new way
-  
+
   data_name <- get_data_name(m1)
   expect_true(file.exists(from_models(data_name)))
 
@@ -56,30 +56,30 @@ test_that("db & plot_iter works",{
       update_dollar_input(rename = c("WT" = "BWT")) %>%
       update_dollar_data(data_name) %>%
       update_ignore("TIME>2") %>%
-      update_sizes("PD = 200") %>% 
+      update_sizes("PD = 200") %>%
       add_cov(param = "K", cov = "WT")
   })
-  
+
   ctl <- ctl %>% gsub_ctl("THEOPP\\.csv", "THEOFILENAME")
-  
+
   expect_true(any(grep("THEOFILENAME", as.character(ctl))))
 
   ctl <- ctl %>% gsub_ctl("THEOFILENAME","THEOPP.csv")
-  
+
   expect_true(!any(grep("THEOFILENAME", as.character(ctl))))
   expect_true(any(grep("THEOPP", as.character(ctl))))
-  
+
   expect_true(any(grepl("KWT-DEFINITION START", ctl_character(ctl))))
-  
+
   ctl_orig <- ctl %>% remove_cov(param = "K", cov = "WT")
-  
+
   expect_true(!any(grepl("KWT-DEFINITION START", ctl_character(ctl_orig))))
 
-  ## build simulation control file  
+  ## build simulation control file
   build_modfile({
     ctl <- m1 %>% change_to_sim %>% change_seed(98765)
   })
-  
+
   expect_true(any(grepl("\\$SIM", rem_comment(ctl_character(ctl)))))
   expect_true(any(grepl("98765", rem_comment(ctl_character(ctl)))))
 
@@ -105,36 +105,36 @@ test_that("db & plot_iter works",{
   ctl <- gsub("sdtab5","sdtab1",ctl) ## create an output conflict
   write(ctl,"Models/run5.mod")
   expect_error(nm("qpsn -m -c auto -t 3000 -- execute run5.mod -dir=5"))
-  
+
   m1scm <- nm("scm run1.mod -config_file=run1.scm -dir=1scm -nmfe_options='-prdefault'")
-  
+
   m1scm_again <- nm("scm run1.mod -config_file=run1.scm -dir=1scm -nmfe_options='-prdefault'")
-  
+
   expect_true(identical(m1scm, m1scm_again))
-  
+
   ## dataset procesing
-  
+
   d <- get_data(m1)
-  
+
   write_derived_data(d, "data")
-  
+
   expect_true(file.exists("DerivedData/data.csv"))
   expect_true(file.exists("DerivedData/data.RData"))
-  
+
   d <- "temp"
   d <- read_derived_data("data")
   expect_true(inherits(d, "data.frame"))
-  
+
   ## post processing
-  
+
   out(m1)
-  
+
   expect_true(inherits(ofv(m1),"numeric"))
-  
+
   do <- nm_output(m1)
   expect_true(inherits(do, "data.frame"))
   expect_true("INNONMEM" %in% names(do))
-  
+
   expect_true(inherits(omega_matrix(m1), "matrix"))
 
   p <- plot_iter(m1,trans = FALSE)
