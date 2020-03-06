b62df90dc2d6fed3c0276d3310db05c5fd9fd1d6
klgk669 <klgk669@rstudio-ha01.scp.astrazeneca.net>
2020-03-06 13:42:31 +0000

    added minimal tests, bumped version

--- tests/testthat/test-run_post.R
+++ tests/testthat/test-run_post.R
@@ -1,4 +1,4 @@
-context("db & plot_iter works")
+context("run and post")
 
 proj_name <- "test_nmproject"
 require(tidyproject)
@@ -9,7 +9,7 @@ cleanup <- function(proj_name){
   if(file.exists(base_proj_name)) unlink(base_proj_name,recursive = TRUE,force = TRUE)
 }
 
-test_that("db & plot_iter works",{
+test_that("run and post",{
   currentwd <- getwd()
   make_project(proj_name)
   on.exit({
@@ -27,27 +27,35 @@ test_that("db & plot_iter works",{
   #m1 <- nm("qpsn -m -c auto -t 3000 -- execute run1.mod -dir=1")
   
   ## dataset procesing
+  ds <- tibble::tibble(run_id = 1)
+  
+  ds$m <- nm2(run_id = ds$run_id) %>%
+    ctl("staging/Models/run{run_id}.mod") %>%
+    #data_path("DerivedData/THEOPP.csv") %>%
+    cmd("execute run{run_id}.mod -dir={run_dir}")
+  
+  expect_message(ds$m %>% run_nm())
+  
+  d <- input_data(ds$m[1])
 
-  d <- input_data(m1)
-
-  write_derived_data(d, "data")
+  write_derived_data_extra(d, "data")
 
   expect_true(file.exists("DerivedData/data.csv"))
   expect_true(file.exists("DerivedData/data.RDS"))
 
-  d <- "temp"
-  d <- read_derived_data("data")
+  d <- read_derived_data_extra("data")
   expect_true(inherits(d, "data.frame"))
 
   ## post processing
 
-  expect_true(inherits(ofv(m1),"numeric"))
-
-  do <- output_table(m1)
+  m1 <- ds$m[1]
+  
+  expect_true(inherits(ofv(ds$m), "numeric"))
+  expect_true(!is.na(ofv(m1)))
+  
+  do <- output_table(m1) %>% dplyr::first()
   expect_true(inherits(do, "data.frame"))
   expect_true("INNONMEM" %in% names(do))
-
-  expect_true(inherits(omega_matrix(m1), "matrix"))
 
   p <- plot_iter(m1,trans = FALSE)
   expect_true(inherits(p,"ggplot"))
