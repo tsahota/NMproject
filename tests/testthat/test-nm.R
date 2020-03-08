894e65a441e90e6fa89e0d655536024f1f2fc171
klgk669 <klgk669@rstudio-ha01.scp.astrazeneca.net>
2020-03-08 11:48:15 +0000

    updated badge added tests

--- tests/testthat/test-nm.R
+++ tests/testthat/test-nm.R
@@ -36,6 +36,9 @@ test_that("nm object basic use",{
   expect_true(is.na(mNA))
   
   m1 <- nm2(run_id = "m1")
+  
+  expect_output(print(m1))
+  
   m1 <- m1 %>% cmd("qpsn -t 100 -c auto -- execute {ctl_name} -dir={run_dir}")
   m1 <- m1 %>% ctl("staging/Models/run1.mod")
   
@@ -51,11 +54,73 @@ test_that("nm object basic use",{
     ))
   )
   
+  expect_true(is.character(status(m1)))
+  
+  browser()
+  
+  m2 <- m1 %>% child(run_id = "m2")
+  
+  ## parent hasn't been run
+  expect_true(is.na(parent(m2)))
+  
+  dtemp <- overlapping_outputs(c(m1,m2))
+  expect_true(nrow(dtemp) == 0)
+  
+  expect_identical(ctl_name(m2), "runm2.mod")
+  
+  ## table renumbering
+  tabs <- psn_exported_files(m2) %>% dplyr::first()  
+  expect_true(
+    any(grepl("sdtabm2", tabs)) & ## contains sdtabm2
+      !any(grepl("sdtabm1", tabs)) ## doesn't contain sdtabm1
+  )
+  
+  m2 <- m2 %>% 
+    run_in("Models/base")
+  
+  expect_identical(ctl_path(m2), "Models/base/runm2.mod")
+  
+  expect_true(file.exists(data_path(m2)))
+  
+  ## read in data
+  d <- input_data(m2)
+  expect_true(is.data.frame(d))
+  
+  nrow(d)
+  d2 <- d %>% dplyr::filter(ID < 12)
+  nrow(d2)
+  expect_true(nrow(d) > nrow(d2))
+  
+  d2$DUMMY <- 1
+  
+  d2 %>% write_derived_data_extra("data")
+  
+  expect_true(file.exists("DerivedData/data.csv"))
+  
+  m2 <- m2 %>% data_path("DerivedData/data.csv")
+  
+  expect_identical(data_path(m2), "DerivedData/data.csv")
+  
+  suppressWarnings({
+    m2 <- m2 %>% fill_input()
+  })
+  
+  expect_true( ## dummy present
+    any(
+      grepl("DUMMY",
+            m2 %>% dollar("INPUT") %>% .[[1]])
+      )
+  )
+
+  m2 <- m2 %>% ignore("ID > 10")
+  d3 <- input_data(m2, filter = TRUE)
+  expect_true(nrow(d2) > nrow(d3))
+  
+  #############################
   #m1 %>% dollar("SUB")
   expect_true(advan(m1) == 2)
   expect_true(trans(m1) == 1)
   
-
   suppressWarnings({
     m1 <- m1 %>% subroutine(advan = 2, trans = 2)
   })
