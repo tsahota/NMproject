d2f71b1ffce32304031b45385ac200636d8c0db8
klgk669 <klgk669@rstudio-ha01.scp.astrazeneca.net>
2020-04-28 17:24:57 +0000

    data_ignore_char recode + data_input drop arg + tests

--- R/NMprojectExtra.R
+++ R/NMprojectExtra.R
@@ -4901,16 +4901,40 @@ data_ignore_char.nm_generic <- function(r, data){
   dol_data <- r %>% dollar("$DATA")
   dol_data <- dol_data[!dol_data %in% ""]
   dol_data <- rem_comment(dol_data)
-  dol_data <- unlist(strsplit(dol_data, split = "\\s"))
+  
+  ###
+  dol_data <- paste(dol_data, collapse = ";")
+  ## remove IGNORE=@
+  dol_data <- gsub("IGNORE\\s*=\\s*@", "", dol_data)
+  ## remove IGNORE=#
+  dol_data <- gsub("IGNORE\\s*=\\s*#", "", dol_data)
   
   ignore_present <- any(grepl(".*IGNORE\\s*=\\s*\\(",dol_data))
   accept_present <- any(grepl(".*ACCEPT\\s*=\\s*\\(",dol_data))
+  
+  
+  ## can now assume that only one is TRUE
   
   type <- NA
   if(ignore_present & accept_present) stop("cannot identify ignore columns")
   if(ignore_present) type <- "IGNORE"
   if(accept_present) type <- "ACCEPT"
-  no_filter <- is.na(type)
+  if(!ignore_present & !accept_present)
+    return("FALSE") ## do not ignore anything
+  
+  ## remove stuff before first IGNORE/ACCEPT
+  dol_data <- gsub(paste0(".*?(",type,".*)"), "\\1", dol_data)
+  ## remove IGNORE=
+  dol_data <- gsub(paste0(type, "\\s*=*"), 
+                   "", dol_data)
+  ## remove brackets
+  dol_data <- gsub("\\(", "", dol_data)
+  dol_data <- gsub("\\)", "", dol_data)
+  ## remove spaces
+  dol_data <- gsub("\\s*", "", dol_data)
+  ## should now be only statemetns with ,; separators
+  
+  dol_data <- unlist(strsplit(dol_data, split = "[,;]"))
   
   if(missing(data)) data <- input_data(r, filter = FALSE, silent = TRUE)
   
@@ -4920,41 +4944,40 @@ data_ignore_char.nm_generic <- function(r, data){
   nonmem_data_names <- gsub("\\$\\w+", "", dollar_input)
   nonmem_data_names <- unlist(strsplit(nonmem_data_names, split = "\\s"))
   nonmem_data_names <- nonmem_data_names[!nonmem_data_names %in% ""]
+  nonmem_data_names <- gsub("(\\w+)=DROP", "\\1", nonmem_data_names)
   nonmem_data_names <- gsub("\\w+=(\\w+)", "\\1", nonmem_data_names)
   #if(length(r_data_names) != length(nonmem_data_names))
   #  stop("length of items in $INPUT doesn't match dataset")
   name_chart <- data.frame(r_data_names, nonmem_data_names, stringsAsFactors = FALSE)
   name_chart <- name_chart[name_chart$r_data_names != name_chart$nonmem_data_names,]
   
-  if(!no_filter){
-    filter_statements <- paste0(".*",type,"\\s*=\\s*\\((\\S[^\\)]+)\\)*.*")
-    dol_data <- dol_data[grepl(filter_statements, dol_data)]
-    filter_statements <- gsub(filter_statements,"\\1",dol_data)
-    filter_statements <- unlist(strsplit(filter_statements,","))
-    filter_statements <- gsub("\\.EQ\\.","==",filter_statements)
-    filter_statements <- gsub("\\.NE\\.","!=",filter_statements)
-    filter_statements <- gsub("\\.EQN\\.","==",filter_statements)
-    filter_statements <- gsub("\\.NEN\\.","!=",filter_statements)
-    filter_statements <- gsub("\\./E\\.","!=",filter_statements)
-    filter_statements <- gsub("\\.GT\\.",">",filter_statements)
-    filter_statements <- gsub("\\.LT\\.","<",filter_statements)
-    filter_statements <- gsub("\\.GE\\.",">=",filter_statements)
-    filter_statements <- gsub("\\.LE\\.","<=",filter_statements)
-    
-    ## substitute names from 
-    for(i in seq_len(nrow(name_chart))){
-      nonmem_data_name <- paste0("\\b", name_chart$nonmem_data_names[i], "\\b")
-      r_data_name <- name_chart$r_data_names[i]
-      filter_statements <- gsub(nonmem_data_name,
-                                r_data_name,
-                                filter_statements)
-    }
-    
-    filter_statements <- paste(filter_statements, collapse= " | ")
-    if("ACCEPT" %in% type) filter_statements <- paste0("!(",filter_statements,")")
-  } else {
-    filter_statements <- "FALSE"
+  #filter_statements <- paste0(".*",type,"\\s*=\\s*\\((\\S[^\\)]+)\\)*.*")
+  #dol_data <- dol_data[grepl(filter_statements, dol_data)]
+  #filter_statements <- gsub(filter_statements,"\\1",dol_data)
+  #filter_statements <- unlist(strsplit(filter_statements,","))
+  filter_statements <- dol_data
+  filter_statements <- gsub("\\.EQ\\.","==",filter_statements)
+  filter_statements <- gsub("\\.NE\\.","!=",filter_statements)
+  filter_statements <- gsub("\\.EQN\\.","==",filter_statements)
+  filter_statements <- gsub("\\.NEN\\.","!=",filter_statements)
+  filter_statements <- gsub("\\./E\\.","!=",filter_statements)
+  filter_statements <- gsub("\\.GT\\.",">",filter_statements)
+  filter_statements <- gsub("\\.LT\\.","<",filter_statements)
+  filter_statements <- gsub("\\.GE\\.",">=",filter_statements)
+  filter_statements <- gsub("\\.LE\\.","<=",filter_statements)
+  
+  ## substitute names from 
+  for(i in seq_len(nrow(name_chart))){
+    nonmem_data_name <- paste0("\\b", name_chart$nonmem_data_names[i], "\\b")
+    r_data_name <- name_chart$r_data_names[i]
+    filter_statements <- gsub(nonmem_data_name,
+                              r_data_name,
+                              filter_statements)
   }
+  
+  filter_statements <- paste(filter_statements, collapse= " | ")
+  if("ACCEPT" %in% type) filter_statements <- paste0("!(",filter_statements,")")
+  
   filter_statements
   
 }
--- R/dollar_input.R
+++ R/dollar_input.R
@@ -1,6 +1,10 @@
 
-dollar_input <- function(d,keep,rename){
-  keep0 <- sapply(d,is_a_number)
+dollar_input <- function(d, keep, drop = character(), rename){
+  
+  ## keep list of names to definately keep
+  ## drop list of columns to definately drop
+  
+  keep0 <- sapply(d,is_a_number)  ## TRUE/FALSE if col is a number or not
   if(missing(keep)){
     message("Excluding all non-numeric items")
     keep <- keep0
@@ -10,11 +14,14 @@ dollar_input <- function(d,keep,rename){
     keep <- names(d) %in% keep
     keep.nonnumeric <- names(d)[keep & !keep0]
     if(length(keep.nonnumeric)>0)
-      warning(paste0("Following non-numerics also dropped: \"",paste(keep.nonnumeric,collapse="\",\""),"\"\n"))
+      message(paste0("Following non-numerics also dropped: \"",paste(keep.nonnumeric,collapse="\",\""),"\"\n"))
     keep <- keep[!keep %in% keep.nonnumeric]
   }
+  
+  if(length(drop) > 0) keep <- keep & !names(keep) %in% drop
+  
   n.remaining <- length(which(keep))
-  if(n.remaining > 45) warning(paste(n.remaining,"non-dropped variables for nonmem. Consider reducing\n"))
+  if(n.remaining > 45) message(paste(n.remaining,"non-dropped variables for nonmem. Consider reducing\n"))
   #if(length(intersect(names(d)[!keep],names(rename)))>0) stop("can't drop renamed columns")
 
   ## rename variables in
@@ -26,23 +33,23 @@ dollar_input <- function(d,keep,rename){
 
   for(i in c("ID","AMT","EVID","CMT","TIME","MDV","RATE","DATE")){
     if(i %in% names(d)[!keep]) {
-      if(i == "DATE") warning("DATE is dropped, NONMEM will try to use this\n")
-      warning(paste("Are you sure",i,"shouldn't be kept?\n"))
+      if(i == "DATE") message("DATE is dropped, NONMEM will try to use this\n")
+      message(paste("Are you sure",i,"shouldn't be kept?\n"))
     }
   }
 
   for(i in c("EXP","DEXP","LOG","RES","WRES","PRED","IPRED","PHI")){
-    if(i %in% names(d)) warning(paste(i,"is already defined by NONMEM. Consider renaming\n"))
+    if(i %in% names(d)) message(paste(i,"is already defined by NONMEM. Consider renaming\n"))
   }
 
   s <- paste0(names(d),ifelse(!keep,"=DROP",""))
   s <- gsub("\\.","",s) ## remove .'s
   s <- toupper(s)
 
-  ## warning when core columns aren't present
+  ## message when core columns aren't present
   core.vars <- c("ID","AMT","EVID","CMT","TIME","MDV")
   cove.vars.missing <- core.vars[!core.vars %in% s]
-  if(length(cove.vars.missing)>0) warning(paste0("Core variables not defined: \"",
+  if(length(cove.vars.missing)>0) message(paste0("Core variables not defined: \"",
                                                  paste(cove.vars.missing,collapse=","),"\"\n"))
 
   s <- paste(s,collapse = " ")
