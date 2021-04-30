cc4dbeab445b540079680fe7c24c132aa93d638b
kjh <lksfj>
2021-04-30 13:55:30 -0400

    improved code library templates for pdf diagnostics

--- inst/extdata/CodeLibrary/Scripts/gof_xpose.R
+++ inst/extdata/CodeLibrary/Scripts/gof_xpose.R
@@ -1,41 +1,30 @@
 ## Description: Function template: Xpose GOF
-## Instructions: source() this file, then run.
+## Instructions: source() this file, then launch on completed model object
 ## Run interactively: FALSE
 ## Key words: function, template
 ## E.g. for sdtab37, run with gof1(37)
 
-gof_xpose <- function(r){
-  
-  #if(NMproject::is_finished(r)) stop("run not yet finished")
-  
-  run.no <- r$run_id
-  directory <- r$run_in
-  
-  ## assumes existance of "plots" directory in main working directory (plots.dir)
-  
+gof_xpose <- function(m){
+
+  wait_finish(m)  ## ensures m is finished
+
   ## assumes the existance of an sdtab $TABLE file e.g.
   ##   $TABLE ID TIME IPRED IWRES IRES CWRES NPDE
   ##   FILE=sdtab[run.no] NOPRINT ONEHEADER FORMAT=tF13.4
-  
-  library(xpose4)
-  
-  xpdb <- xpose.data(run.no,directory=paste0(directory,"/"))
-  
-  ### Make any changes to xpdb, e.g. to change the independent variable
-  ## change.xvardef(xpdb,var="idv") <- "TRLD"
-  
-  pdf(file.path("Results",paste("gof.xpose.run.",run.no,".xpose.basic.pdf",sep="")))
-  
-  ## Delete/Modify the following as needed
-  print(dv.vs.pred(xpdb,smooth=NULL,type="p",main=NULL, logy=F,logx=F,
-                   ylb="Plasma concentration (units)"))
-  print(dv.vs.ipred(xpdb,smooth=NULL,type="p",main=NULL, logy=F,logx=F,
-                    ylb="Plasma concentration (units)"))
-  print(cwres.vs.pred(xpdb,smooth=NULL,type="p",main=NULL))
-  print(cwres.vs.idv(xpdb,smooth=NULL,type="p",main=NULL,xlb="Time (units)"))
-  print(ind.plots(xpdb,layout=c(2,2),ylb="Plasma concentration (units)",
-                  xlb="Time (units)"))
+
+
+  library(xpose)
+
+  xpdb <- xpose_data(runno = run_id(m), dir = run_in(m))
+
+  pdf(file.path(results_dir(m1), paste0("gof_xpose_run_",run_id(m),".pdf")))
+
+  print(dv_vs_pred(xpdb))
+  print(dv_vs_ipred(xpdb))
+  print(pred_vs_idv(xpdb))
+  print(ipred_vs_idv(xpdb))
   
   dev.off()
-  
+
+
 }
