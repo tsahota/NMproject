e1324cafdc0a545e98a49e31763ff1c18498966d
kjh <lksfj>
2021-06-01 08:57:54 -0400

    demo update

--- inst/extdata/examples/theopp/R/AUC.R
+++ inst/extdata/examples/theopp/R/AUC.R
@@ -1,19 +1,56 @@
-## Copied from staging/localpackage/R/AUC.R
-##  (2021-04-16 11:12:35) by tarjinde
-## Copied from staging/localpackage/R/AUC.R
-##  (2021-03-02 13:53:56) by tarjinde
-## Copied from staging/localpackage/R/AUC.R
-##  (2020-04-03 18:18:14) by klgk669
-## Copied from /home/klgk669/poppk_20180926_pk-eff-ae-interim2/Scripts/AUC.R
-##  (2019-01-23 14:32:02) by klgk669
-## Copied from /home/klgk669/poppk_20180522_pk-eff-ae_cut28Feb2018/Scripts/AUC.R
-##  (2018-10-11 12:58:58) by klgk669
-## Copied from /projects/qcp/QCP_MODELING/PMXcodelibrary/R/AUC.R
-##  (2018-08-15 15:26:51) by klgk669
-## Description: Basic NCA AUC function
-## Key words: NCA, AUC, function
-## Author: Tarj Sahota, Peter Lawrence
-## Run interactively: FALSE
+#' @title Calculate AUC
+#'
+#' @description Computes AUC by trapezoidal rule.
+#'
+#'   The style of documentation is from `roxygen2`.  It is how package
+#'   documentation is created and may be of interest for those users looking to
+#'   turn their functions into shareable packages.  The learning curve is fairly
+#'   large though so if this isn't a strong interest, don't worry about
+#'   including these kinds of comments for your functions.  You can leave them
+#'   undocumented or document them in a more informal way.  Documenting your
+#'   functions in this style is optional but it will enable use of `?AUC` to
+#'   allow yourself and others to browse documentation and make it easy to
+#'   incorporate your function into a package at a later date if you believe it
+#'   will be useful to others.
+#'
+#'   Also available is the `testthat` package.  If you want you can construct
+#'   automated unit tests for this function.
+#'
+#'   All of this is optional though, you can just leave functions undocumented
+#'   if you want.
+#'
+#' @author Tarj Sahota, Peter Lawrence
+#'
+#' @param time Numeric vector of times to integrate over.
+#' @param conc Numeric vector of concentrations to integrate.
+#' @param loq Numeric value (default = 0).  Conc values below this will be set
+#'   to 0.
+#' @param method Character indicating calculation method of trapezoidal area.
+#'
+#' @return Will return a single number for the AUC.  This function is most
+#'   useful using `dplyr`.
+#'
+#' @seealso [NMproject::ppc_data()]
+#' @examples
+#'
+#' \dontrun{
+#'
+#' AUC(d$TIME, d$DV)  ## return AUC number
+#'
+#' ## data.frame with AUC value for each ID (one row per ID)
+#' d %>% group_by(ID) %>%
+#'   summarise(AUC = AUC(TIME, DV),
+#'             CMAX = max(DV),
+#'             TMAX = TIME[which.max(DV)])
+#'
+#' ## Add exposure columns to a dataset
+#' d %>% group_by(ID) %>%
+#'   mutate(AUC = AUC(TIME, DV),
+#'             CMAX = max(DV),
+#'             TMAX = TIME[which.max(DV)])
+#'
+#' }
+#' @export
 
 AUC <- function(time, conc, loq=0,method=c("linuplogdown","linuplogdowntmax","linear"))
 {
@@ -21,13 +58,13 @@ AUC <- function(time, conc, loq=0,method=c("linuplogdown","linuplogdowntmax","li
   trap.log.rule <- function(x,y){ ## custom function for log down
     y1 <- y[-length(y)]
     y2 <- y[-1]
-    sum(diff(x)*(y1-y2)/(log(y1)-log(y2)))      
+    sum(diff(x)*(y1-y2)/(log(y1)-log(y2)))
   }
   ok = !is.na(time) & !is.na(conc) & conc >= loq
-  
+
   time <- time[ok]
   conc <- conc[ok]
-  
+
   if(method=="linear") return(Hmisc::trap.rule(time, conc))
   if(method=="linuplogdowntmax"){
     tmax <- time[which.max(conc)]
