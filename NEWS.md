a0704fd5de51331b290bed826d14a957c78c2b95
tarjinde <tarjinder.z.sahota@gsk.com>
2021-08-18 09:41:24 -0400

    dev version + cran_note_handle()

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+# NMproject (development version)
+
 # NMproject 0.6.4
 
 * Revamped the manual edit interface.  This is backwards incompatible.
--- R/NMproject-package.R
+++ R/NMproject-package.R
@@ -69,5 +69,13 @@
 ## usethis namespace: end
 NULL
 
-# Suppress R CMD check note
-#' @importFrom lifecyle badge
+# Function to suppress R CMD check notes
+cran_note_handle <- function(){
+  
+  lifecycle::badge
+  
+  invisible()
+  
+}
+
+
